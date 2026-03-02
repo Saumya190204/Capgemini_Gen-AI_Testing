e52891214c632e278adeb0ba6215a5f2acd5d1f0
Saumya190204 <Saumya190204@gmail.com>
2026-03-02 08:25:57 +0530

    File Committed

---
+++ Capgemini Daily Assessment/DAY-01.py
@@ -0,0 +1,11 @@
+class DefaultSolution:
+    
+    def calculate_bill(self, units):
+        if units <= 100:
+            bill = units * 5
+        elif units <= 300:
+            bill = (100 * 5) + ((units - 100) * 7)
+        else:
+            bill = (100 * 5) + (200 * 7) + ((units - 300) * 10)
+        
+        return bill
